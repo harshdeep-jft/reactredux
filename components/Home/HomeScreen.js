b987fb854427bd4a664fc44288d79f646ab7e9a0
harshdeep-jft <harshdeep.saiyam@jellyfishtechnologies.com>
2021-03-18 12:51:58 +0530

    :sparkles: HomeScreen State
    added mapstate in homeScreen

--- components/Home/HomeScreen.js
+++ components/Home/HomeScreen.js
@@ -3,10 +3,10 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 
 
-function HomeScreen({ navigation }) {
+function HomeScreen({ navigation, ...props }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'orange' }}>
-            <Text>Home Screen</Text>
+            <Text>Home Screen {props.name ? 'true' : 'false'}</Text>
             <Button title='Next Screen' onPress={() => {
                 console.log('----->', navigation);
                 navigation.navigate('Information')
@@ -17,8 +17,13 @@ function HomeScreen({ navigation }) {
 }
 
 const mapStateToProps = (state) => {
-    return {}
+    console.log('statess--->', state.infoReducer);
+    return {
+        name: state.infoReducer.name,
+        age: state.infoReducer.age,
+    }
 }
+
 
 const mapDispatchToProps = (dispatch) => {
     return {}
